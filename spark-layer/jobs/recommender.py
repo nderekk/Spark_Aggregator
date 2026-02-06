3b0e2656df1ae129289454ca82be3bd8e298528f
Nikos <nickdereken@gmail.com>
2026-02-06 23:30:50 +0200

    vectorization complete + updated requirements. #TODO actual similarity logic

--- spark-layer/jobs/recommender.py
+++ spark-layer/jobs/recommender.py
@@ -9,7 +9,14 @@ spark = SparkSession.builder \
   .config("spark.mongodb.read.connection.uri", atlas_uri) \
   .config("spark.mongodb.read.database", "test") \
   .config("spark.mongodb.read.collection", "courses") \
-  .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:10.3.0") \
+  .config("spark.jars.packages", 
+    "org.mongodb.spark:mongo-spark-connector_2.12:10.3.0,"
+    "com.johnsnowlabs.nlp:spark-nlp_2.12:5.5.1") \
+  .config("spark.driver.host", "127.0.0.1") \
+  .config("spark.driver.bindAddress", "127.0.0.1") \
+  .config("spark.driver.memory", "4g") \
+  .config("spark.kryoserializer.buffer.max", "2000M") \
+  .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
   .getOrCreate()
   
 raw_df = spark.read.format("mongodb")\
@@ -34,3 +41,80 @@ cleaned_df = raw_df.select(
 
 print(f"Rows remaining after robust filter: {cleaned_df.count()}")
 cleaned_df.show(5)
+
+
+# spark nlp pipeline
+from sparknlp.base import DocumentAssembler, Finisher
+from sparknlp.annotator import (Lemmatizer, Stemmer, 
+                                Tokenizer, Normalizer,
+                                StopWordsCleaner)
+from sparknlp.pretrained import PretrainedPipeline
+
+# this is the documents
+document_assembler = DocumentAssembler() \
+  .setInputCol("text_content") \
+  .setOutputCol("document") \
+  .setCleanupMode("shrink")
+document_assembler.transform(cleaned_df).select("document")
+
+# these are the annotators
+# split to tokens
+tokenizer = Tokenizer() \
+  .setInputCols(["document"]) \
+  .setOutputCol("token")
+  
+# clean unwanted characters and grabvrage
+normalizer = Normalizer() \
+  .setInputCols(["token"]) \
+  .setOutputCol("normalized") \
+  .setLowercase(True)
+
+# remove stopwords
+stopwords_cleaner = StopWordsCleaner() \
+  .setInputCols(["normalized"]) \
+  .setOutputCol("cleaned_tokens") \
+  .setCaseSensitive(False)
+  
+# stem the words to bring them to the root form
+stemmer = Stemmer() \
+  .setInputCols(["cleaned_tokens"]) \
+  .setOutputCol("stem")
+  
+# convert the stemmed tokens back to token array 
+finisher = Finisher() \
+  .setInputCols(["stem"]) \
+  .setOutputCols(["tokens"]) \
+  .setOutputAsArray(True) \
+  .setCleanAnnotations(False)
+    
+from pyspark.ml import Pipeline
+
+nlp_pipeline = Pipeline(stages=[
+  document_assembler,
+  tokenizer,
+  normalizer,
+  stopwords_cleaner,
+  stemmer,
+  finisher
+])
+
+nlp_model = nlp_pipeline.fit(cleaned_df)
+processed_df = nlp_model.transform(cleaned_df)
+
+tokens_df = processed_df.select("course_id", "tokens")
+
+from pyspark.ml.feature import CountVectorizer, IDF
+from pyspark.sql import functions as fun
+
+cv = CountVectorizer(inputCol="tokens", outputCol="raw_features")
+cv_model = cv.fit(tokens_df)
+vectorized_tokens = cv_model.transform(tokens_df)
+
+idf = IDF(inputCol="raw_features", outputCol="features")
+idf_model = idf.fit(vectorized_tokens)
+vectorized_df = idf_model.transform(vectorized_tokens)
+
+vectorized_df = vectorized_df.drop(fun.col('raw_features'))
+
+print(f"Total rows in vectorized_df: {vectorized_df.count()}")
+vectorized_df.show(5)
