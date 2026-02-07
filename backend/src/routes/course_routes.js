7254bddcaade3a3da21270a0dab7100b4610e65f
Nikos <nickdereken@gmail.com>
2026-02-07 23:29:53 +0200

    backend reccomendation endpoint done, merging and integrating frontend

--- backend/src/routes/course_routes.js
+++ backend/src/routes/course_routes.js
@@ -4,7 +4,7 @@ const courseController = require('../controllers/course_controller');
 const verifyToken = require('../middleware/auth');
 
 router.get('/',  courseController.getAllCourses);
-router.get('/:id/similar', verifyToken, courseController.getSimilarCourses);
+router.get('/:id/similar',  courseController.getSimilarCourses);
 router.get('/sync/:source', verifyToken, courseController.syncSource);
 
 router.post('/', verifyToken,courseController.createCourse);
